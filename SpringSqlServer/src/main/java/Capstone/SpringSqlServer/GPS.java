699fb05af79d6f593f4ba09ff821debc60f0de59
Lucas Montanari <lmontanari@csumb.edu>
2019-03-18 21:55:12 -0700

    added branch

--- SpringSqlServer/src/main/java/Capstone/SpringSqlServer/GPS.java
+++ SpringSqlServer/src/main/java/Capstone/SpringSqlServer/GPS.java
@@ -5,5 +5,18 @@ import javax.persistence.Entity;
 @Entity
 public class GPS {
 
+    private Float xcoord;
+    private Float ycoords;
 
+    public Float getXcoord() {
+        return this.xcoord;
+    }
+
+    public void setXcoord(Float xcoord) {
+        this.xcoord = xcoord;
+    }
+
+    public Float getYcoord() {
+        return this.ycoords;
+    }
 }
